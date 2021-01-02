5bae5fb27340b31aa76361df1cddd2ae7c971900
Upul Bandara <upul.bandara@airudi.com>
2021-01-01 22:50:34 -0500

    fixed some importing issues

--- etl.py
+++ etl.py
@@ -24,8 +24,8 @@ from pyspark.sql.types import DateType, IntegerType, TimestampType
 config = configparser.ConfigParser()
 config.read("dl.cfg")
 
-os.environ["AWS_ACCESS_KEY_ID"] = config["AWS_ACCESS_KEY_ID"]
-os.environ["AWS_SECRET_ACCESS_KEY"] = config["AWS_SECRET_ACCESS_KEY"]
+os.environ["AWS_ACCESS_KEY_ID"] = config["AWS"]["AWS_ACCESS_KEY_ID"]
+os.environ["AWS_SECRET_ACCESS_KEY"] = config["AWS"]["AWS_SECRET_ACCESS_KEY"]
 
 
 def create_spark_session():
